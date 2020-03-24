8d93897743754fb723bca23276aaf25ba39ed03b
Luke <Lukasronsholt@gmail.com>
2020-03-24 11:02:07 +0100

    Update datetime.dart

--- route_app/lib/core/extensions/datetime.dart
+++ route_app/lib/core/extensions/datetime.dart
@@ -1,6 +1,6 @@
 /// DateTime Extension
 extension DateTimeExtension on DateTime {
-  ///
+  /// Parse a DateTime object to a String, if null returns empty string
   String parseToString() {
     if (this == null) {
       return '';
