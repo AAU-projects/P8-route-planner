6ea38b2c92b36dfc43c044e667340a1f07f3f8e6
Jonas <grann.alsen@gmail.com>
2020-04-07 14:23:09 +0200

    Fix tests

--- route_app/test/layout/screens/home_test.dart
+++ route_app/test/layout/screens/home_test.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mockito/mockito.dart';
+import 'package:route_app/core/services/API/logging_service.dart';
+import 'package:route_app/core/services/database.dart';
+import 'package:route_app/core/services/interfaces/API/logging.dart';
 import 'package:route_app/layout/screens/home.dart';
 import 'package:route_app/layout/widgets/fields/search_text_field.dart';
+import 'package:route_app/locator.dart';
+
+class MockDatabase extends Mock implements DatabaseService {}
+class MockLogging extends Mock implements LoggingService {}
 
 void main() {
-  setUp(() {});
+  setUp(() {
+    final MockDatabase db = MockDatabase();
+    final MockLogging mockLog = MockLogging();
+    locator.reset();
+    locator.registerSingleton<DatabaseService>(db);
+    locator.registerSingleton<LoggingAPI>(mockLog);
+  });
 
   testWidgets('Screen renders', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: HomeScreen()));
