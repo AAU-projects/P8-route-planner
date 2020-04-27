ab1c52008cdd0588d573a486d450f7782b41df67
Rasmus Egholm Nielsen <th3nielsen@hotmail.com>
2020-04-27 13:27:04 +0200

    Create trips.dart

---
+++ route_app/lib/layout/screens/trips.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+///
+class TripsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(child: Text('HEJ MAGNUS')),
+    );
+  }
+}
