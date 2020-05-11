7ff9fca0d1fedfd3a2568bb8b521df1c54e47bd7
Jonas <grann.alsen@gmail.com>
2020-05-11 14:15:56 +0200

    Make linter happy
    Co-Authored-By: alechj <alechj@users.noreply.github.com>

--- route_app/lib/layout/screens/home.dart
+++ route_app/lib/layout/screens/home.dart
@@ -596,7 +596,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: FloatingActionButton(
           heroTag: 'near_me',
           backgroundColor: colors.SearchBackground,
-          onPressed: () => {
+          onPressed: () => <void>{
             locationModel.updateCurrentLocation(),
             _centerMap(locationModel.currentLocationObj)},
           child: const Icon(Icons.near_me, size: 25, color: colors.Text),
